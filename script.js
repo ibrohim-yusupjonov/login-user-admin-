a63a33240290dc5db4fa8c964781c3c4899e5005
ibrohim-yusupjonov <110836470+ibrohim-yusupjonov@users.noreply.github.com>
2022-12-06 22:32:08 +0500

    user

---
+++ script.js
@@ -0,0 +1,16 @@
+function func(){
+    let savol = prompt(" login: admin / user");
+    let javob = document.getElementById('javob');
+    if ( savol == 'admin' || savol == 'ADMIN' || savol == 'Admin'){
+        let parol = prompt('parolni kiriting');
+        console.log('parol :' + parol)
+        // javob.innerHTML = parol;
+    }   else if( savol == undefined || savol == null || savol == '' ){
+        javob.innerHTML = "Canceled"
+    }   else if( savol == 'user' ){
+        javob.innerHTML = 'Welcome user :)'
+    }   else if (savol ){
+    javob.innerHTML = 'Bunday login mavjud emas'
+    }
+}
+func()
